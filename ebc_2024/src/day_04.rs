8a55041158bc79db4ec668fb8d879e99cb6a2524
Michael Conrad <mmitton@gmail.com>
2024-11-19 11:00:39 -0500

    2024 - 4

--- ebc_2024/src/day_04.rs
+++ ebc_2024/src/day_04.rs
@@ -2,29 +2,66 @@
 use helper::{print, println, Error, HashMap, HashSet, Lines, LinesOpt};
 
 #[derive(Default)]
-pub struct Day04 {}
+pub struct Day04 {
+    nails: Vec<isize>,
+}
 
 impl Day04 {
     pub fn new() -> Self {
         Self::default()
     }
 
+    fn level_to(&self, level: isize) -> usize {
+        self.nails
+            .iter()
+            .map(|nail| (nail - level).unsigned_abs())
+            .sum()
+    }
+
+    fn level(&self) -> usize {
+        let min = self.nails.iter().copied().min().unwrap();
+        self.level_to(min)
+    }
+
     fn part1(&mut self) -> Result<helper::RunOutput, Error> {
-        Err(Error::Unsolved)
+        Ok(self.level().into())
     }
 
     fn part2(&mut self) -> Result<helper::RunOutput, Error> {
-        Err(Error::Unsolved)
+        Ok(self.level().into())
     }
 
     fn part3(&mut self) -> Result<helper::RunOutput, Error> {
-        Err(Error::Unsolved)
+        let min = self.nails.iter().copied().min().unwrap();
+        let max = self.nails.iter().copied().max().unwrap();
+        let mut level_to = (max + min) / 2;
+        let mut level_to_strikes = self.level_to(level_to);
+        loop {
+            let lower = self.level_to(level_to - 1);
+            let higher = self.level_to(level_to + 1);
+
+            if lower < level_to_strikes {
+                level_to -= 1;
+                level_to_strikes = lower;
+                continue;
+            }
+            if higher < level_to_strikes {
+                level_to += 1;
+                level_to_strikes = higher;
+                continue;
+            }
+            break;
+        }
+        Ok(level_to_strikes.into())
     }
 }
 
 impl helper::Runner for Day04 {
     fn parse(&mut self, file: &[u8], _part: u8) -> Result<(), Error> {
-        let _lines = Lines::from_bufread(file, LinesOpt::RAW)?;
+        let lines = Lines::from_bufread(file, LinesOpt::RAW)?;
+        for line in lines.iter() {
+            self.nails.push(line.parse()?);
+        }
         Ok(())
     }
 
