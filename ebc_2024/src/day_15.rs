33026613a1869b2c04871819d34826d62c01886c
Michael Conrad <mmitton@gmail.com>
2024-11-24 03:50:27 -0500

    2024 - 15

--- ebc_2024/src/day_15.rs
+++ ebc_2024/src/day_15.rs
@@ -1,13 +1,24 @@
 #[allow(unused_imports)]
-use helper::{print, println, Error, HashMap, HashSet, Lines, LinesOpt};
-use std::collections::{BTreeMap, BTreeSet, VecDeque};
+use helper::{print, println, Error, HashMap, HashSet, Lines, LinesOpt, Point};
+use std::{
+    collections::{BTreeMap, VecDeque},
+    ops::RangeInclusive,
+};
+
+#[derive(Copy, Clone)]
+struct Path {
+    to: Point<u8>,
+    to_picked_up: u32,
+    to_last_pickup: u32,
+    to_dist: usize,
+}
 
 #[derive(Default)]
 pub struct Day15 {
     map: Vec<Vec<char>>,
-    start: (usize, usize),
-    herbs: HashMap<char, Vec<(usize, usize)>>,
-    paths: HashMap<(usize, usize), Vec<((usize, usize), u32, u32, usize)>>,
+    start: Point<u8>,
+    herbs: HashMap<char, Vec<Point<u8>>>,
+    paths: HashMap<Point<u8>, Vec<Path>>,
 }
 
 impl Day15 {
@@ -15,8 +26,8 @@ impl Day15 {
         Self::default()
     }
 
-    fn paths_from(&self, from: (usize, usize)) -> Vec<((usize, usize), u32, u32, usize)> {
-        let start = self.map[from.1][from.0];
+    fn paths_from(&self, from: Point<u8>) -> Vec<Path> {
+        let start = self.map[from.y as usize][from.x as usize];
         let mut work = VecDeque::new();
         work.push_front((0, from, 0));
         let mut seen = HashSet::default();
@@ -25,21 +36,26 @@ impl Day15 {
         let mut paths = Vec::new();
         while let Some((dist, at, picked_up)) = work.pop_front() {
             for next in [
-                (at.0 - 1, at.1),
-                (at.0 + 1, at.1),
-                (at.0, at.1 - 1),
-                (at.0, at.1 + 1),
+                Point::new(at.x - 1, at.y),
+                Point::new(at.x + 1, at.y),
+                Point::new(at.x, at.y - 1),
+                Point::new(at.x, at.y + 1),
             ] {
                 if !seen.insert(next) {
                     continue;
                 }
-                match self.map[next.1][next.0] {
+                match self.map[next.y as usize][next.x as usize] {
                     '.' => work.push_back((dist + 1, next, picked_up)),
                     c if c.is_ascii_alphabetic() && c != start => {
                         let last_pickup = 1u32 << (c as u8 - b'A');
                         if picked_up & last_pickup == 0 {
                             let picked_up = picked_up | last_pickup;
-                            paths.push((next, picked_up, last_pickup, dist + 1));
+                            paths.push(Path {
+                                to: next,
+                                to_picked_up: picked_up,
+                                to_last_pickup: last_pickup,
+                                to_dist: dist + 1,
+                            });
                         }
                         work.push_back((dist + 1, next, picked_up));
                     }
@@ -50,15 +66,33 @@ impl Day15 {
         paths
     }
 
-    fn find_paths(&mut self) {
-        self.start = (self.map[0].iter().position(|c| *c == '.').unwrap(), 1);
-        self.map[0][self.start.0] = '#';
-        self.map[1][self.start.0] = 'Z';
+    fn find_paths(&mut self, start: Option<Point<u8>>, x_range: Option<RangeInclusive<usize>>) {
+        if let Some(start) = start {
+            self.start = start;
+        } else {
+            self.start = Point::new(self.map[0].iter().position(|c| *c == '.').unwrap() as u8, 1);
+            self.map[0][self.start.x as usize] = '#';
+            self.map[1][self.start.x as usize] = 'Z';
+        }
 
+        let x_range = if let Some(x_range) = x_range {
+            x_range
+        } else {
+            0..=self.map[0].len()
+        };
+
+        self.herbs.clear();
+        self.paths.clear();
         for (y, row) in self.map.iter().enumerate() {
             for (x, c) in row.iter().enumerate() {
+                if !x_range.contains(&x) {
+                    continue;
+                }
                 if c.is_ascii_alphabetic() {
-                    self.herbs.entry(*c).or_default().push((x, y));
+                    self.herbs
+                        .entry(*c)
+                        .or_default()
+                        .push(Point::new(x as u8, y as u8));
                 }
             }
         }
@@ -73,26 +107,22 @@ impl Day15 {
     }
 
     fn find_full_path(&mut self) -> Result<usize, Error> {
-        println!("Making paths");
-        self.find_paths();
-        println!("Paths made");
-
-        let mut work: BTreeMap<usize, HashSet<((usize, usize), u32)>> = BTreeMap::default();
+        let mut work: BTreeMap<usize, HashSet<(Point<u8>, u32)>> = BTreeMap::default();
         work.entry(2)
             .or_default()
             .insert((self.start, 1u32 << (b'Z' - b'A')));
 
-        let mut seen: HashMap<((usize, usize), u32), usize> = HashMap::default();
+        let mut seen: [HashMap<u32, usize>; 255 * 255] =
+            std::array::from_fn(|_| Default::default());
 
         while let Some((dist, cur_work)) = work.pop_first() {
-            println!("{dist}");
             for (at, picked_up) in cur_work {
                 if picked_up.count_ones() as usize == self.herbs.len() {
                     if at == self.start {
                         return Ok(dist);
                     }
                     // return home
-                    for (to, _, _, to_dist) in self.paths.get(&at).unwrap().iter().copied() {
+                    for Path { to, to_dist, .. } in self.paths.get(&at).unwrap().iter().copied() {
                         if to != self.start {
                             continue;
                         }
@@ -103,8 +133,12 @@ impl Day15 {
                     continue;
                 }
 
-                for (to, to_picked_up, to_last_pickup, to_dist) in
-                    self.paths.get(&at).unwrap().iter().copied()
+                for Path {
+                    to,
+                    to_picked_up,
+                    to_last_pickup,
+                    to_dist,
+                } in self.paths.get(&at).unwrap().iter().copied()
                 {
                     if picked_up & to_last_pickup != 0 {
                         continue;
@@ -116,7 +150,8 @@ impl Day15 {
                     let at = to;
                     let picked_up = picked_up | to_picked_up;
 
-                    let seen = seen.entry((at, picked_up)).or_insert(usize::MAX);
+                    let seen_idx = (at.y as usize * 255) + at.x as usize;
+                    let seen = seen[seen_idx].entry(picked_up).or_insert(usize::MAX);
                     if *seen > dist {
                         *seen = dist;
                         work.entry(dist).or_default().insert((at, picked_up));
@@ -157,11 +192,40 @@ impl Day15 {
     }
 
     fn part2(&mut self) -> Result<helper::RunOutput, Error> {
+        self.find_paths(None, None);
         Ok(self.find_full_path()?.into())
     }
 
     fn part3(&mut self) -> Result<helper::RunOutput, Error> {
-        Ok(self.find_full_path()?.into())
+        if self.map[0].len() != 255 {
+            self.find_paths(None, None);
+            Ok(self.find_full_path()?.into())
+        } else {
+            // split in to three columns
+            self.map[75][84] = 'Z';
+            self.map[75][85] = '#';
+            self.map[75][86] = 'Y';
+            self.map[75][168] = 'X';
+            self.map[75][169] = '#';
+            self.map[75][170] = 'Z';
+
+            let start = Point::new(self.map[0].iter().position(|c| *c == '.').unwrap() as u8, 1);
+            self.map[0][start.x as usize] = '#';
+            self.map[1][start.x as usize] = 'Z';
+
+            let mut min_dist = 4;
+
+            for (start, x_range) in [
+                (Point::new(84, 75), 0..=84),
+                (Point::new(170, 75), 170..=255),
+                (start, 86..=168),
+            ] {
+                self.find_paths(Some(start), Some(x_range));
+                min_dist += self.find_full_path()?;
+            }
+
+            Ok(min_dist.into())
+        }
     }
 }
 
