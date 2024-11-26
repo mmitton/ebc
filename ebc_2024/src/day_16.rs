ce91cfbeb6e22bdfe1329c6898f6e4a291074c1d
Michael Conrad <mmitton@gmail.com>
2024-11-26 06:43:38 -0500

    Optimize

--- ebc_2024/src/day_16.rs
+++ ebc_2024/src/day_16.rs
@@ -1,11 +1,58 @@
 #[allow(unused_imports)]
 use helper::{print, println, Error, HashMap, HashSet, Lines, LinesOpt};
 
+#[derive(Debug)]
+struct Wheel {
+    spin: u8,
+    slots: Vec<String>,
+    map: Vec<[u8; 2]>,
+}
+
+impl Wheel {
+    fn new(spin: u8) -> Self {
+        Self {
+            spin,
+            slots: Vec::new(),
+            map: Vec::new(),
+        }
+    }
+
+    fn get(&self, idx: u8) -> (&str, &[u8; 2]) {
+        (&self.slots[idx as usize], &self.map[idx as usize])
+    }
+
+    fn add_slot(&mut self, slot: &str, map: &mut HashMap<char, u8>) {
+        let slot = slot.trim();
+        if slot.is_empty() {
+            return;
+        }
+        self.slots.push(slot.into());
+        let mut chars = slot.chars();
+        let first = chars.next().unwrap();
+        let last = chars.last().unwrap();
+
+        let first = if let Some(first) = map.get(&first) {
+            *first
+        } else {
+            let next = map.len() as u8;
+            map.insert(first, next);
+            next
+        };
+        let last = if let Some(last) = map.get(&last) {
+            *last
+        } else {
+            let next = map.len() as u8;
+            map.insert(last, next);
+            next
+        };
+
+        self.map.push([first, last]);
+    }
+}
+
 #[derive(Default)]
 pub struct Day16 {
-    spins: Vec<usize>,
-    wheels: Vec<Vec<String>>,
-    wheel_maps: Vec<Vec<Vec<u8>>>,
+    wheels: Vec<Wheel>,
 }
 
 impl Day16 {
@@ -13,50 +60,42 @@ impl Day16 {
         Self::default()
     }
 
-    fn sequence(&self, offset: &[usize]) -> String {
+    fn sequence(&self, offset: &[u8; 12]) -> String {
         let mut ans = String::new();
-        for (idx, wheel) in self.wheels.iter().enumerate() {
+        for (idx, (wheel, offset)) in self.wheels.iter().zip(offset.iter()).enumerate() {
             if idx != 0 {
                 ans.push(' ');
             }
-            ans.push_str(wheel[offset[idx] % wheel.len()].as_str());
+            let (slot, _) = wheel.get(*offset);
+            ans.push_str(slot);
         }
         ans
     }
 
-    fn spin(&self, offset: &mut [usize]) {
-        for (idx, spin) in self.spins.iter().enumerate() {
-            offset[idx] = (offset[idx] + spin) % self.wheels[idx].len();
-        }
-    }
-
-    fn score(&self, offset: &[usize]) -> usize {
-        let mut num = [0usize; 256];
-        let mut min = u8::MAX;
-        let mut max = 0;
-        for (idx, wheel_maps) in self.wheel_maps.iter().enumerate() {
-            let wheel_map = &wheel_maps[offset[idx]];
-            for n in wheel_map.iter().step_by(2).copied() {
-                min = min.min(n);
-                max = max.max(n);
+    fn score(&self, offset: &[u8; 12]) -> usize {
+        let mut num = [0u8; 64];
+        for (wheel, offset) in self.wheels.iter().zip(offset.iter()) {
+            let (_, map) = wheel.get(*offset);
+            for n in map.iter().copied() {
                 num[n as usize] += 1;
             }
         }
 
-        let take = (max - min) as usize + 1;
-        let min = min as usize;
         num.iter()
-            .skip(min)
-            .take(take)
             .copied()
-            .map(|n| n.saturating_sub(2))
+            .map(|n| n.saturating_sub(2) as usize)
             .sum()
     }
 
     fn part1(&mut self) -> Result<helper::RunOutput, Error> {
-        let mut offset = vec![0; self.spins.len()];
+        let mut offset = [0u8; 12];
         for _ in 0..100 {
-            self.spin(&mut offset);
+            offset
+                .iter_mut()
+                .zip(self.wheels.iter())
+                .for_each(|(offset, wheel)| {
+                    *offset = (*offset + wheel.spin).rem_euclid(wheel.slots.len() as u8)
+                });
         }
 
         Ok(self.sequence(&offset).into())
@@ -65,16 +104,21 @@ impl Day16 {
     fn part2(&mut self) -> Result<helper::RunOutput, Error> {
         let mut score = 0;
         let mut seen = HashMap::default();
-        let mut offset = vec![0; self.spins.len()];
+        let mut offset = [0u8; 12];
         const ITERS: usize = 202420242024;
-        seen.insert(offset.clone(), (0, 0));
+        seen.insert(offset, (0, 0));
         let mut spin = 0;
         let mut found_cycle = false;
         while spin < ITERS {
-            self.spin(&mut offset);
+            offset
+                .iter_mut()
+                .zip(self.wheels.iter())
+                .for_each(|(offset, wheel)| {
+                    *offset = (*offset + wheel.spin).rem_euclid(wheel.slots.len() as u8)
+                });
             score += self.score(&offset);
             if !found_cycle {
-                if let Some((last_spin, last_score)) = seen.insert(offset.clone(), (spin, score)) {
+                if let Some((last_spin, last_score)) = seen.insert(offset, (spin, score)) {
                     let cycle_len = spin - last_spin + 1;
                     let left = ITERS - spin;
                     let cycles = left / cycle_len;
@@ -94,30 +138,32 @@ impl Day16 {
         #[derive(Hash, PartialEq, Eq)]
         struct Work {
             score: usize,
-            offset: Vec<usize>,
+            offset: [u8; 12],
         }
 
         let mut work_hash = HashSet::default();
         work_hash.insert(Work {
             score: 0,
-            offset: vec![0; self.spins.len()],
+            offset: [0u8; 12],
         });
 
         let mut next_hash = HashSet::default();
 
         const ITERS: usize = 256;
         for _ in 0..ITERS {
-            for Work { score, offset } in work_hash.iter() {
+            for Work { score, offset } in work_hash.drain() {
                 macro_rules! process {
                     ($delta:expr) => {{
                         let mut offset = offset.clone();
-                        for i in 0..self.wheels.len() {
-                            offset[i] = (offset[i] as isize + $delta)
-                                .rem_euclid(self.wheels[i].len() as isize)
-                                as usize;
-                        }
 
-                        self.spin(&mut offset);
+                        offset
+                            .iter_mut()
+                            .zip(self.wheels.iter())
+                            .for_each(|(offset, wheel)| {
+                                *offset = (*offset as i8 + $delta + wheel.spin as i8)
+                                    .rem_euclid(wheel.slots.len() as i8)
+                                    as u8
+                            });
                         let score = score + self.score(&offset);
                         next_hash.insert(Work { score, offset });
                     }};
@@ -129,7 +175,6 @@ impl Day16 {
             }
 
             std::mem::swap(&mut work_hash, &mut next_hash);
-            next_hash.clear();
         }
 
         let mut min = usize::MAX;
@@ -147,23 +192,17 @@ impl helper::Runner for Day16 {
     fn parse(&mut self, file: &[u8], _part: u8) -> Result<(), Error> {
         let lines = Lines::from_bufread(file, LinesOpt::RAW)?;
         for spin in lines[0].split(',') {
-            self.spins.push(spin.parse()?);
+            self.wheels.push(Wheel::new(spin.parse()?));
         }
-        let wheel_cnt = (lines[2].len() + 1) / 4;
-        self.wheels.extend((0..wheel_cnt).map(|_| Vec::new()));
-        self.wheel_maps.extend((0..wheel_cnt).map(|_| Vec::new()));
+
+        let mut map = HashMap::default();
         for line in lines[2..].iter() {
-            for i in 0..wheel_cnt {
+            for (i, wheel) in self.wheels.iter_mut().enumerate() {
                 let idx = i * 4;
                 if idx >= line.len() {
                     break;
                 }
-                let wheel = line[idx..idx + 3].trim();
-                let wheel_map = wheel.chars().map(|c| c as u8).collect::<Vec<u8>>();
-                if !wheel.is_empty() {
-                    self.wheels[i].push(wheel.into());
-                    self.wheel_maps[i].push(wheel_map);
-                }
+                wheel.add_slot(&line[idx..idx + 3], &mut map);
             }
         }
         Ok(())
